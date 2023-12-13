1d0a05e15d0f1598c1cd86d828e32431e4e659e5
arun <arun@yahoo.com>
2023-12-13 08:20:10 +0000

    prometheus port open

--- main.tf
+++ main.tf
@@ -48,6 +48,16 @@ resource "aws_security_group" "frontend" {
   }
 
 
+  #------promethous port----------
+
+  ingress {
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
